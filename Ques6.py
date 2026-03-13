652b780b9cabd7256764b8abea3aaa0da2112120
krishnachhkara <krishnachhikara47@gmail.com>
2026-03-13 15:25:14 +0530

    adding que6

---
+++ Ques6.py
@@ -0,0 +1,9 @@
+"""
+Given the code below, add the correct step as the third argument of the
+rangef) function on line 1 in order to return [-75, -60, -45, -30] when converted
+to a list.
+my_range = range(-75, -25, )
+"""
+
+my_range = range(-75, -25,15 )#just minus the number like difference between 75 and 60 is of 15 its your step
+print(list(my_range))
