4aee7f936fc04848eacf853d6ae7df5ef8fb1ef2
krishnachhkara <krishnachhikara47@gmail.com>
2026-03-13 15:37:17 +0530

    adding que1

---
+++ Dictionary/Ques1.py
@@ -0,0 +1,14 @@
+"""Given the code below, use the correct code on line 3 in order to return
+the value associated with key 4. Do not use a method/function as a solution for this
+exercise!
+crypto = {1: "Bitcoin", 2: "Ethereum", 3: "Litecoin", 4: "Stellar",
+5: "XRP"}
+value =
+print(value)"""
+
+
+crypto = {1: "Bitcoin", 2: "Ethereum", 3: "Litecoin", 4: "Stellar",
+5: "XRP"}
+
+value = crypto[4]
+print(value)
