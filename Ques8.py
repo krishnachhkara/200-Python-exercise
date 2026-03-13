a6bca2c64fd7888c93d2334874615e10279bc3e9
krishnachhkara <krishnachhikara47@gmail.com>
2026-03-13 15:31:57 +0530

    adding que8

---
+++ Ques8.py
@@ -0,0 +1,7 @@
+""" Given the code below, write the correct range on line 1 in order to return
+[-10] when converted to a list.
+my_range =
+"""
+
+my_range = range(-10,-11,-1)#step can be negative also. in range normally range(1,5)means start from 1 then 2,3,4,5 here step +1 is silent like you are infront of your gf. so it means increase in start either +/- doesn't matter
+print(list(my_range))
