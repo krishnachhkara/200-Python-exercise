6488f0078c980fe952c2f635b4849ae2e9d3f21f
krishnachhkara <krishnachhikara47@gmail.com>
2026-03-13 15:27:00 +0530

    adding que7

---
+++ Ques7.py
@@ -0,0 +1,7 @@
+"""Given the code below, add the correct step as the third argument of
+the range() function on line 1 in order to return [-25, 5,35, 65,
+95,125] when converted to a list.
+my_range = range(-25, 139, )"""
+
+my_range = range(-25, 139,30 )
+print(list(my_range))
