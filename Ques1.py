24e4d90eab75f30a8f57c8203a9df25ee9d74bfe
krishnachhkara <krishnachhikara47@gmail.com>
2026-03-13 15:03:20 +0530

    adding 2 questions

---
+++ Ques1.py
@@ -0,0 +1,12 @@
+"""
+1. Given the code below, use the correct argument(s) for the range()
+function on line 1 in order to return a range of consecutive integers
+from 0 to 9 inclusively. Use a single argument inside the parentheses
+of range()!
+2.
+my_range =
+print(list(my_range)) #[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+"""
+
+my_range = range(10)
+print(list(my_range))#run without list and with to see the difference
