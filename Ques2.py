73d8411e07d4a177c79f532a285e6449fed36730
krishnachhkara <krishnachhikara47@gmail.com>
2026-03-13 15:14:18 +0530

    adding que3

---
+++ Ques2.py
@@ -0,0 +1,9 @@
+"""
+2. Given the code below, use the correct argument(s) for the range() function
+on line 1 in order to return a range of consecutive integers from 0 to 9
+inclusively. Use two arguments inside the parentheses of range()!
+"""
+
+
+my_range = range(0,10)#two args
+print(list(my_range))
---
+++ Ques3.py
@@ -0,0 +1,8 @@
+"""
+3. Given the code below, use the correct argument(s) for the range() function
+on line 1 in order to return a range of consecutive integers from 117 to 129(exclusively).
+my_range =
+"""
+my_range = range(117,129)# works for 1 to n-1
+print(list(my_range))
+
