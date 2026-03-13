155579679fdee24aed4867c1f565881f8d6d39d0
krishnachhkara <krishnachhikara47@gmail.com>
2026-03-13 15:17:32 +0530

    adding que4

---
+++ Ques4.py
@@ -0,0 +1,10 @@
+"""
+Given the code below, use the correct argument(s) for the range() function on
+line 1 in order to return [10,13,16,19] when converted to a list.
+my_range =
+"""
+
+my_range = range(10,20,3)#3 here is step or jump which works as n-1 same like stop
+# range (start,stop,jump) both stop and jump work as n-1
+
+print(list(my_range))
