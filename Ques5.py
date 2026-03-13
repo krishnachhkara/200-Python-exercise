e7310cb84b438c48274834d624523fcaf4fde162
krishnachhkara <krishnachhikara47@gmail.com>
2026-03-13 15:22:06 +0530

    adding que5

---
+++ Ques5.py
@@ -0,0 +1,8 @@
+"""
+Given the code below, add the correct step as the third
+argument of the rangef) function on line 1 in order to return [115,120] when converted to a list.
+my_range = range(115, 125, )
+"""
+
+my_range = range(115, 125, 5 )#remember start is not counted but stop is counted
+print(list(my_range))
