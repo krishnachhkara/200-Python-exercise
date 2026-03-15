852291e91233ddfb7b7e4580ee797e24ad499255
krishnachhkara <krishnachhikara47@gmail.com>
2026-03-15 12:19:22 +0530

    adding que11

---
+++ Dictionary/Ques11.py
@@ -0,0 +1,13 @@
+"""Given the code below, use the correct function on line 3 in
+order to get the sum of all the keys in the dictionary.
+crypto = {1: "Bitcoin", 2: "Ethereum", 3: "Litecoin", 4: "Stellar",
+5: "XRP"}
+add =
+print(add)"""
+
+crypto = {1: "Bitcoin", 2: "Ethereum", 3: "Litecoin", 4: "Stellar",
+5: "XRP"}
+
+add = sum(crypto)#using sum function as method use is not specified
+
+print(add)
