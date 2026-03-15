916d0a6197b5b93ce28a3bcdf69a1b03443dd673
krishnachhkara <krishnachhikara47@gmail.com>
2026-03-15 11:55:53 +0530

    adding Que3

---
+++ Dictionary/Ques3.py
@@ -0,0 +1,13 @@
+""" Given the code below, use the correct code on line 3 in order
+to update the value associated with key 4 to "Cardano".
+crypto = {1: "Bitcoin", 2: "Ethereum", 3: "Litecoin", 4:
+"Stellar", 5: "XRP"}
+print(crypto[4])
+"""
+
+crypto = {1: "Bitcoin", 2: "Ethereum", 3: "Litecoin", 4:
+"Stellar", 5: "XRP"}
+
+crypto[4] = "Cardano"
+
+print(crypto[4])
